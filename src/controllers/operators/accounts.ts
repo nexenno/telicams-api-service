e45faed25a44fb9b43f4ff30ac72cd4de986ec72
Increase <engr.increase@gmail.com>
2026-02-23 18:02:13 +0100

    fixed rider doc update error

--- src/controllers/operators/accounts.ts
+++ src/controllers/operators/accounts.ts
@@ -17,6 +17,13 @@ export default class OperatorAccountController extends SimpleNodeJsController {
       delete: OperatorAccountService.DeleteOperatorUser,
     })
   }
+  async changePassword() {
+    if (this.method !== "put") return helpers.outputError(this.res, 405)
+    return OperatorAccountService.ChangeAdminPassword({
+      customData: this._custom_data, body: this.body,
+      req: this.req, res: this.res, query: this.query,
+    })
+  }
 
   async teamLists(id: string | undefined) {
     if (id && helpers.isInvalidID(id)) return helpers.outputError(this.res, 404)
